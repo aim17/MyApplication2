3d7bbf80e36647f6a4f6fa9f58779c88cfe10509
aim17 <aim17email@gmail.com>
2019-03-20 16:27:39 -0400

    Added user customizable Add Task button

--- app/src/main/java/com/aim17/myapplication/activity/MainActivity.java
+++ app/src/main/java/com/aim17/myapplication/activity/MainActivity.java
@@ -1,10 +1,15 @@
 package com.aim17.myapplication.activity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.InputType;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -53,11 +58,46 @@ public class MainActivity extends AppCompatActivity {
 
     public void addTask(View view)
     {
-        Task task4 = new Task();
-        task4.setName("Add Task Button Test");
-        task4.setDetails("Description");
-        taskList.add(task4);
-        adapter.notifyDataSetChanged();
+        final Task task = new Task();
+        //task4.setName("Add Task Button Test");
+        //task4.setDetails("Description");
+        //taskList.add(task4);
+        //adapter.notifyDataSetChanged();
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Add Task");
+
+    // Set up the input
+        final EditText input = new EditText(this);
+        final EditText input1 = new EditText(this);
+
+        LinearLayout alertLayout = new LinearLayout(this);
+        alertLayout.setOrientation(LinearLayout.VERTICAL);
+        alertLayout.addView(input);
+        alertLayout.addView(input1);
+        builder.setView(alertLayout);
+
+    // Set up the buttons
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String taskTitle = input.getText().toString();
+                String taskDesc = input1.getText().toString();
+                task.setName(taskTitle);
+                task.setDetails(taskDesc);
+                taskList.add(task);
+                adapter.notifyDataSetChanged();
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
+
 
     }
 }
